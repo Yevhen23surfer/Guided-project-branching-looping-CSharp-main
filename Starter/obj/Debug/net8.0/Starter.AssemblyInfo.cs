d5f134ceef3bad6334cb9fe883d308e58e5d48a0
Yevhen Surfer <23skopets@gmail.com>
2024-04-03 13:09:48 +0200

    Display all pet characteristics for pets with data assigned

--- Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f57df57746a78a9a2ee1aa6136b3b6874d330d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9f413098dc1c028745700783d1d52fad01ced4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
